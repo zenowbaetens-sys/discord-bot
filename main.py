eedc81ca3f1a9e1013af707903a573ac25740261
zenowbaetens-sys <zenow.baetens@gmail.com>
2026-04-17 01:33:04 +0200

    Add basic Discord bot with ping command
    Niks

---
+++ main.py
@@ -0,0 +1,21 @@
+import discord
+import os
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'Logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content == "!ping":
+        await message.channel.send("Pong!")
+
+client.run(os.getenv("TOKEN"))
